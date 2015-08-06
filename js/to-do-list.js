de2e8534a3ce54a7c11444b9fc9b9bbfe2028d47
Lyle Scott <Lyle.C.Scott@gmail.com>
2015-08-06 02:08:59 -0700

    Save button functionality.

--- js/to-do-list.js
+++ js/to-do-list.js
@@ -40,11 +40,16 @@ var deleteTask = function(index) {
 var listTasks = function() {
   $('#tasks').empty();
   $.each(Tasklist.list, function(i, val) {
-    $('#tasks').append('<div class=\"to-do-item\" id=\"box' + i + '\"><textarea id=\"item' + i + '\">' + Tasklist.list[i].task + '</textarea><button type=\"button\" id=\"save' + i + '\">SAVE</button><button type=\"button\" id=\"done' + i + '\">DONE</button><button type=\"button\" id=\"delete' + i + '\">DELETE</button></div>');
+    $('#tasks').prepend('<div class=\"to-do-item\" id=\"box' + i + '\"><textarea id=\"item' + i + '\">' + Tasklist.list[i].task + '</textarea><button type=\"button\" id=\"save' + i + '\">SAVE</button><button type=\"button\" id=\"done' + i + '\">DONE</button><button type=\"button\" id=\"delete' + i + '\">DELETE</button></div>');
       if (Tasklist.list[i].done) {
         $('#item' + i).css("background-color", "#D3D9EE");
         $('#box' + i).css("background-color", "#D3D9EE");
       };
+      $('#item' + i).change(function() {
+        console.log("change!");
+        $('#item' + i).css("color", "#D3D9EE");
+        $('#save' + i).css("background-color", "#C79DB4").css("border", "5px solid #F9FDEF");
+      })
     save(i);
     done(i);
     deleteTask(i);
