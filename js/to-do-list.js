729aabf65c0541daa6a0aad667fbfda38f664031
Lyle Scott <Lyle.C.Scott@gmail.com>
2015-08-05 18:24:53 -0700

    HTML & CSS both look good. Adding tasks and rendering both work. Need to activate buttons rendered inside tasks on the list.

--- js/to-do-list.js
+++ js/to-do-list.js
@@ -1,5 +1,36 @@
+'use strict'
 
+var Task = {
+list: []
+};
 
-var newTask = function() {
+var enter = function() {
 
 };
+
+var update = function() {
+
+};
+
+var done = function() {
+
+};
+
+var deleteTask = function() {
+
+};
+
+var listTasks = function() {
+  $('#tasks').empty();
+  $.each(Task.list, function(i, val) {
+    $('#tasks').append('<div class=\"to-do-item\"><textarea>' + Task.list[i] + '</textarea><button type=\"button\" id=\"update' + i + '\">UPDATE</button><button type=\"button\" id=\"done' + i + '\">DONE</button><button type=\"button\" id=\"delete' + i + '\">DELETE</button></div>')
+  })
+};
+
+$('#submit-task').on({'click': function() {
+  if ($('#new-task').val() !== "") {
+    Task.list.push($('#new-task').val());
+    $('#new-task').val("");
+    listTasks();
+  }
+}});
