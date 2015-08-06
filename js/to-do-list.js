63ae984b8244f4c09552ae5cb51f8431bd292a78
Lyle Scott <Lyle.C.Scott@gmail.com>
2015-08-05 18:46:19 -0700

    update buttons work.

--- js/to-do-list.js
+++ js/to-do-list.js
@@ -4,12 +4,11 @@ var Task = {
 list: []
 };
 
-var enter = function() {
-
-};
-
-var update = function() {
-
+var update = function(index) {
+  $('#update' + index).on({'click': function() {
+    Task.list[index] = ($('#item' + index).val());
+    console.log(Task.list[index]);
+  }})
 };
 
 var done = function() {
@@ -23,7 +22,8 @@ var deleteTask = function() {
 var listTasks = function() {
   $('#tasks').empty();
   $.each(Task.list, function(i, val) {
-    $('#tasks').append('<div class=\"to-do-item\"><textarea>' + Task.list[i] + '</textarea><button type=\"button\" id=\"update' + i + '\">UPDATE</button><button type=\"button\" id=\"done' + i + '\">DONE</button><button type=\"button\" id=\"delete' + i + '\">DELETE</button></div>')
+    $('#tasks').append('<div class=\"to-do-item\"><textarea id=\"item' + i + '\">' + Task.list[i] + '</textarea><button type=\"button\" id=\"update' + i + '\">UPDATE</button><button type=\"button\" id=\"done' + i + '\">DONE</button><button type=\"button\" id=\"delete' + i + '\">DELETE</button></div>');
+    update(i);
   })
 };
 
