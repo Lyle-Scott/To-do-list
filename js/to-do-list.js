fb216815d8f1dd618d803feae45b5a800e9b9aa2
Lyle Scott <Lyle.C.Scott@gmail.com>
2015-08-05 22:04:49 -0700

    Everything is working. Just features from this point on.

--- js/to-do-list.js
+++ js/to-do-list.js
@@ -1,41 +1,52 @@
 'use strict'
 
-var Task = {
+var Tasklist = {
 list: []
 };
 
+var Item = function (task) {
+ this.task = task;
+ this.done = false;
+}
+
 var update = function(index) {
   $('#update' + index).on({'click': function() {
-    Task.list[index] = ($('#item' + index).val());
-    console.log(Task.list[index]);
+    Tasklist.list[index].task = ($('#item' + index).val());
+    console.log(Tasklist.list[index].task);
   }})
 };
 
 var done = function(index) {
   $('#done' + index).on({'click': function() {
-    console.log('clicked');
-    if ($("#item" + index).css('background-color')=="rgb(1, 153, 116)") {
+    console.log('clicked', Tasklist.list[index].done);
+    if (Tasklist.list[index].done == false) {
+      Tasklist.list[index].done = true;
       $('#item' + index).css("background-color", "#4C1200");
       $('#box' + index).css("background-color", "#4C1200");
       console.log("changed to dark red");
     } else {
       $('#item' + index).css("background-color", "#019974");
       $('#box' + index).css("background-color", "#019974");
+      Tasklist.list[index].done = false;
     }
   }})
 };
 
 var deleteTask = function(index) {
   $('#delete' + index).on({'click': function() {
-    Task.list.splice(index, 1);
+    Tasklist.list.splice(index, 1);
     listTasks();
   }})
 };
 
 var listTasks = function() {
   $('#tasks').empty();
-  $.each(Task.list, function(i, val) {
-    $('#tasks').append('<div class=\"to-do-item\" id=\"box' + i + '\"><textarea id=\"item' + i + '\">' + Task.list[i] + '</textarea><button type=\"button\" id=\"update' + i + '\">UPDATE</button><button type=\"button\" id=\"done' + i + '\">DONE</button><button type=\"button\" id=\"delete' + i + '\">DELETE</button></div>');
+  $.each(Tasklist.list, function(i, val) {
+    $('#tasks').append('<div class=\"to-do-item\" id=\"box' + i + '\"><textarea id=\"item' + i + '\">' + Tasklist.list[i].task + '</textarea><button type=\"button\" id=\"update' + i + '\">UPDATE</button><button type=\"button\" id=\"done' + i + '\">DONE</button><button type=\"button\" id=\"delete' + i + '\">DELETE</button></div>');
+      if (Tasklist.list[i].done) {
+        $('#item' + i).css("background-color", "#4C1200");
+        $('#box' + i).css("background-color", "#4C1200");
+      };
     update(i);
     done(i);
     deleteTask(i);
@@ -44,7 +55,8 @@ var listTasks = function() {
 
 $('#submit-task').on({'click': function() {
   if ($('#new-task').val() !== "") {
-    Task.list.push($('#new-task').val());
+    var input = new Item($('#new-task').val());
+    Tasklist.list.push(input);
     $('#new-task').val("");
     listTasks();
   }
